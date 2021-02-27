6a8870c0137c41655dd98309f7810025791aeb66
Mcfloy <lucasperreau@gmail.com>
2021-02-27 16:58:07 +0100

    Add the rest of the project...

---
+++ README.md
@@ -0,0 +1,50 @@
+# Vault Credentials
+Rust Library that fetch secrets from Vault and load them as environment variables.
+Inspired by [Spring Cloud Vault](https://cloud.spring.io/spring-cloud-vault/reference/html/#vault.config.authentication).
+
+## Getting started
+We will assume that you want to retrieve some secrets from your local [Vault Server](https://learn.hashicorp.com/tutorials/vault/getting-started-dev-server?in=vault/getting-started).
+
+This is the json secret located in `secret/hello` (from Vault perspective, either by using the Vault UI or Vault CLI)
+```json
+{
+  "my-key": "my-value",
+  "github.com": {
+    "api-key": "123456",
+    "base-url": "http://localhost:8080"
+  }
+}
+```
+
+In your program you must provide the environment variables required to make a connection to the Vault Server and retrieve the token.
+You can use the [.dotenv](https://crates.io/crates/dotenv) crate and put the variables in a *.env* file.
+```
+VAULT_ADDR=http://127.0.0.1:8200
+VAULT_PATH=hello
+VAULT_TYPE=approle
+VAULT_ROLE_ID=9bf0581f-[...]-533ba207ec80
+VAULT_SECRET_ID=55473ff2-[...]-0ab9ae6e499b
+```
+
+To use the vault_credentials crate in your program, import it and call the `initialize` method.
+```rust
+use dotenv::dotenv;
+
+fn main() {
+    dotenv().ok();
+    vault_credentials::initialize();
+
+    println!("{}", std::env::var("github.com.api-key").unwrap());
+    // Output: 123456
+}
+```
+
+## Authentication types
+You can use other types of authentication by using `VAULT_TYPE`. (default is set to `token`)
+
+|Vault Type|Required environment variables|
+|---|---|
+|`token`|`VAULT_TOKEN`|
+|`approle`|`VAULT_ROLE_ID`,`VAULT_SECRET_ID`|
+|`kubernetes`|To be implemented|
+|`userpass`,`ldap` |`VAULT_USERNAME`, `VAULT_PASSWORD`|
---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+use std::env;
+use serde_json::json;
+use reqwest;
+
+pub fn initialize() {
+   retrieve_token();
+
+   env_setter();
+}
+
+pub fn retrieve_token() {
+    let vault_addr = env::var("VAULT_ADDR")
+        .expect("Cannot get environment variable VAULT_ADDR");
+
+    let authentication_type = match env::var("VAULT_TYPE") {
+        Ok(value) => value,
+        _ => String::from("token")
+    };
+    let request_url = format!("{}/v1/auth/{}/login", vault_addr, authentication_type);
+
+    match authentication_type.as_str() {
+        "token" => {
+            env::var("VAULT_TOKEN")
+                .expect("Cannot get environment variable VAULT_TOKEN");
+        },
+        "approle" => {
+            let role_id = env::var("VAULT_ROLE_ID")
+                .expect("Cannot get environment variable VAULT_ROLE_ID");
+            let secret_id = env::var("VAULT_SECRET_ID")
+                .expect("Cannot get environment variable VAULT_SECRET_ID");
+
+            let payload = json!({
+                "role_id": role_id,
+                "secret_id": secret_id
+            });
+
+            call_vault_login(&request_url, &payload);
+        }
+        "userpass" | "ldap" => {
+            let username = env::var("VAULT_USERNAME")
+                .expect("Cannot get environment variable VAULT_USERNAME");
+            let password = env::var("VAULT_PASSWORD")
+                .expect("Cannot get environment variable VAULT_PASSWORD");
+
+            let payload = json!({
+                "username": username,
+                "password": password
+            });
+
+            call_vault_login(&request_url, &payload);
+        }
+        _ => panic!("{} is not supported.", authentication_type)
+    }
+}
+
+fn call_vault_login(request_url: &str, payload: &serde_json::Value) {
+    println!("Calling vault login {} with payload {:?}", request_url, payload);
+
+    let response: serde_json::Value = reqwest::blocking::Client::new()
+        .post(request_url)
+        .json(payload)
+        .send()
+        .unwrap()
+        .json()
+        .unwrap();
+
+    if let Some(errors) = response.get("errors") {
+        panic!("Cannot retrieve token: {}", errors.as_array().unwrap().first().unwrap());
+    }
+
+    let client_token = response
+        .get("auth").unwrap()
+        .get("client_token").unwrap();
+    env::set_var("VAULT_TOKEN", client_token.as_str().unwrap());
+}
+
+pub fn env_setter() {
+    let vault_addr = std::env::var("VAULT_ADDR").unwrap();
+    let vault_token = std::env::var("VAULT_TOKEN").unwrap();
+    let vault_path = std::env::var("VAULT_PATH").unwrap();
+
+    let request_uri = format!("{}/v1/secret/data/{}", vault_addr, vault_path);
+
+    let response: serde_json::Value = reqwest::blocking::Client::new()
+        .get(&request_uri)
+        .header("X-Vault-Token", vault_token)
+        .send()
+        .unwrap()
+        .json()
+        .unwrap();
+
+    if let Some(errors) = response.get("errors") {
+        panic!("Cannot retrieve token: {}", errors.as_array().unwrap().first().unwrap());
+    }
+
+    let object = response.get("data").unwrap().get("data").unwrap().as_object().unwrap();
+
+    for (key, value) in object {
+        add_to_env(key, value);
+    }
+}
+
+fn add_to_env(key_path: &String, value: &serde_json::Value) {
+    if value.is_object() {
+        for (key, value) in value.as_object().unwrap() {
+            add_to_env(&format!("{}.{}", key_path, key), value);
+        }
+    } else if !value.is_array() {
+        env::set_var(key_path, value.as_str().unwrap());
+    } else {
+        unimplemented!("Vault secrets shouldn't have arrays and must use objects to ensure unique keys.");
+    }
+}
